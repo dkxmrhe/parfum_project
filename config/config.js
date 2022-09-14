601bd6c4023f68b11aef28e4274ae22a48def766
dkxmrhe <dkxmrhe1@daum.net>
2022-09-14 16:44:27 +0900

    delete timezone

--- config/config.js
+++ config/config.js
@@ -7,7 +7,6 @@ module.exports = {
       database: "parfum_database",
       host: "127.0.0.1",
       dialect: "mysql",
-      timezone: "Asia/Seoul"
     },
     test: {
       username: "root",
@@ -15,7 +14,6 @@ module.exports = {
       database: "parfum_database_test",
       host: "127.0.0.1",
       dialect: "mysql",
-      timezone: "Asia/Seoul"
     },
     production: {
       username: "root",
@@ -24,6 +22,5 @@ module.exports = {
       host: "127.0.0.1",
       dialect: "mysql",
       logging: false,
-      timezone: "Asia/Seoul"
     }
 }
