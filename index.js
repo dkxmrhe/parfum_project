ef5adf43e942ae7edaf44bfd65c039e2bd8383d8
dkxmrhe <dkxmrhe1@daum.net>
2022-09-14 21:16:21 +0900

    express

--- index.js
+++ index.js
@@ -55,8 +55,8 @@ if(process.env.NODE_ENV === 'production') {
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/uploads', express.static('uploads'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use(cookieParser(process.env.COOKIE_SECRET));
 const sessionOption = {
