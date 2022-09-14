58368117dad71866747722a86596c5a7ec689181
dkxmrhe <dkxmrhe1@daum.net>
2022-09-14 19:41:30 +0900

    config change

--- config/config.js
+++ config/config.js
@@ -18,7 +18,7 @@ module.exports = {
     production: {
       username: "root",
       password: process.env.SEQUELIZE_PASSWORD,
-      database: "database_production",
+      database: "parfum_database",
       host: "127.0.0.1",
       dialect: "mysql",
       logging: false,
